c9b9c299429b3c3f139856b94735b51fda268d23
holyangel <slverwolf@gmail.com>
2016-06-12 12:03:26 -0500

    sched: Reduce latency for better responsiveness
    Signed-off-by: HolyAngel <slverwolf@gmail.com>
    Signed-off-by: Salllz <sal235222727@gmail.com>
    Signed-off-by: alanndz <alanndz7@gmail.com>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    (cherry picked from commit 75bf13c9f82eb88f5d578b6bb3022092768d985a)
    Signed-off-by: Panchajanya1999 <kernel@panchajanya.dev>
    Signed-off-by: fjrXTR <fajarslebew31@gmail.com>
    Signed-off-by: Ramadhani <ramadhanifals@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -59,15 +59,15 @@ unsigned int sysctl_sched_tunable_scaling = SCHED_TUNABLESCALING_LINEAR;
 /*
  * Minimal preemption granularity for CPU-bound tasks:
  *
- * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
+ * (default: 0.5 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity			= 750000ULL;
-static unsigned int normalized_sysctl_sched_min_granularity	= 750000ULL;
+unsigned int sysctl_sched_min_granularity			= 500000ULL;
+static unsigned int normalized_sysctl_sched_min_granularity	= 500000ULL;
 
 /*
  * This value is kept at sysctl_sched_latency/sysctl_sched_min_granularity
  */
-static unsigned int sched_nr_latency = 8;
+static unsigned int sched_nr_latency = 6;
 
 /*
  * After fork, child runs first. If set to 0 (default) then
@@ -136,9 +136,9 @@ int __weak arch_asym_cpu_priority(int cpu)
  * to consumption or the quota being specified to be smaller than the slice)
  * we will always only issue the remaining available time.
  *
- * (default: 5 msec, units: microseconds)
+ * (default: 4 msec, units: microseconds)
  */
-unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
+unsigned int sysctl_sched_cfs_bandwidth_slice		= 4000UL;
 #endif
 
 static inline void update_load_add(struct load_weight *lw, unsigned long inc)
