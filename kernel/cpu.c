52428624064b8764208e82240859c3797a5bde01
idkwhoiam322 <idkwhoiam322@raphielgang.org>
2020-01-15 05:06:35 +0530

    treewide: silence some excessive logging

--- kernel/cpu.c
+++ kernel/cpu.c
@@ -1567,7 +1567,7 @@ int freeze_secondary_cpus(int primary)
 	 */
 	cpumask_clear(frozen_cpus);
 
-	pr_info("Disabling non-boot CPUs ...\n");
+	pr_debug("Disabling non-boot CPUs ...\n");
 	for_each_online_cpu(cpu) {
 		if (cpu == primary)
 			continue;
@@ -1623,7 +1623,7 @@ void thaw_secondary_cpus(void)
 	if (cpumask_empty(frozen_cpus))
 		goto out;
 
-	pr_info("Enabling non-boot CPUs ...\n");
+	pr_debug("Enabling non-boot CPUs ...\n");
 
 	arch_thaw_secondary_cpus_begin();
 
