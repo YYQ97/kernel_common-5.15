3b1ab1d69573499bcc9e7d0b0ff76ed43e9d69f6
Tyler Nijmeh <tylernij@gmail.com>
2020-02-02 22:22:46 -0800

    sched/core: Minimize number of tasks to load balance
    We don't want sched_nr_migrate to be too high, as that would impact
    real-time latencies as the load balancing process for SCHED_OTHER
    disallows IRQ interrupts.
    
    Instead of making this a constant value, let's use an amount that
    relates to the CPU performance of a device.
    
    Consider that a device with a very weak 4 core processor needs to load
    balance 32 tasks from the most busy-but-idle CPU. That balancing would
    take significantly longer than a device with 8 cores.
    
    On the contrary view, that same 8 core device balancing 32 tasks would
    have no issues with SCHED_OTHER performance, however, real-time tasks
    would suffer more jitter than is necessary.
    
    Let's only balance as many tasks as there are CPUs in a device for
    optimal SCHED_OTHER performance and SCHED_FIFO / SCHED_RR latency.
    
    Change-Id: I4b2681cb51450006b6bcf93bc9994d1bf07fd23e
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: Richard Raya <rdxzv.dev@gmail.com>
    Signed-off-by: Ramadhani <ramadhanifals@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -89,7 +89,7 @@ __read_mostly int sysctl_resched_latency_warn_once = 1;
  * Number of tasks to iterate in a single balance run.
  * Limited because this is done with IRQs disabled.
  */
-const_debug unsigned int sysctl_sched_nr_migrate = 32;
+const_debug unsigned int sysctl_sched_nr_migrate = NR_CPUS;
 
 /*
  * period over which we measure -rt task CPU usage in us.
