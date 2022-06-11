bec96a10742bf17dece951f02c372bd871ccfd1d
Michael Bestas <mkbestas@lineageos.org>
2022-06-12 00:59:45 +0300

    arm64: Fix CONFIG_64BIT leak to userspace
    Commit c7c0eecf894c3e353499fffd6f2ddaefc89bbd4a disabled the use of config
    options in uapi headers. Use __aarch64__ which is a compiler-defined
    method of obtaining the architecture instead of CONFIG_64BIT.
    
    Fixes the following error:
        error: kernel/google/raviole/arch/arm64/include/uapi/asm/sigcontext.h:
            leak CONFIG_64BIT to user-space
    
    Change-Id: Ie1f22679882acbc3e253316d5281287956a4febd
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- arch/arm64/include/uapi/asm/sigcontext.h
+++ arch/arm64/include/uapi/asm/sigcontext.h
@@ -17,7 +17,7 @@
 #ifndef _UAPI__ASM_SIGCONTEXT_H
 #define _UAPI__ASM_SIGCONTEXT_H
 
-#ifdef CONFIG_64BIT
+#ifdef __aarch64__
 #ifndef __ASSEMBLY__
 
 #include <linux/types.h>
@@ -267,7 +267,7 @@ struct za_context {
 
 #define SVE_SIG_CONTEXT_SIZE(vq) \
 		(SVE_SIG_REGS_OFFSET + SVE_SIG_REGS_SIZE(vq))
-#else /* CONFIG_64BIT */
+#else /* __aarch64__ */
 
 /*
  * Signal context structure - contains all info to do with the state
@@ -297,7 +297,7 @@ struct sigcontext {
 	unsigned long arm_cpsr;
 	unsigned long fault_address;
 };
-#endif /* CONFIG_64BIT */
+#endif /* __aarch64__ */
 
 /*
  * If the ZA register is enabled for the thread at signal delivery then,
