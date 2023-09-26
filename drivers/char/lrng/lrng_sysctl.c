c8f709b17ebe15348ee6e0f776eb5d198aaafa78
yyq97 <332235240@qq.com>
2023-09-26 10:33:48 +0800

    lrng: patch from https://github.com/smuellerDD/lrng/tree/master/backports/v51-5.15.131

--- drivers/char/lrng/lrng_sysctl.c
+++ drivers/char/lrng/lrng_sysctl.c
@@ -86,7 +86,7 @@ void lrng_sysctl_update_max_write_thresh(u32 new_digestsize)
 	mb();
 }
 
-static struct ctl_table random_table[] = {
+struct ctl_table random_table[] = {
 	{
 		.procname	= "poolsize",
 		.maxlen		= sizeof(int),
@@ -130,10 +130,3 @@ static struct ctl_table random_table[] = {
 		.extra1		= &lrng_drng_reseed_max_min,
 	},
 };
-
-static int __init random_sysctls_init(void)
-{
-	register_sysctl_init("kernel/random", random_table);
-	return 0;
-}
-device_initcall(random_sysctls_init);
