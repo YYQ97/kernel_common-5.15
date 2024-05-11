db5a6588b92aa55e2a24719ab714c971dbbf3d66
yyq97 <332235240@qq.com>
2024-05-12 04:48:39 +0800

    Revert "ssg-iosched.c: fix error"
    This reverts commit e5dd6dd19bd27c5cf9ad5acf8d181c563981fbac.

--- block/ssg-iosched.c
+++ block/ssg-iosched.c
@@ -20,8 +20,6 @@
 #include <linux/rbtree.h>
 #include <linux/sbitmap.h>
 
-#include <trace/events/block.h>
-
 #include "blk.h"
 #include "blk-mq.h"
 #include "blk-mq-debugfs.h"
@@ -676,7 +674,6 @@ static void ssg_insert_request(struct blk_mq_hw_ctx *hctx, struct request *rq,
 	struct request_queue *q = hctx->queue;
 	struct ssg_data *ssg = q->elevator->elevator_data;
 	const int data_dir = rq_data_dir(rq);
-	LIST_HEAD(free);
 
 	/*
 	 * This may be a requeue of a write request that has locked its
@@ -684,12 +681,10 @@ static void ssg_insert_request(struct blk_mq_hw_ctx *hctx, struct request *rq,
 	 */
 	blk_req_zone_write_unlock(rq);
 
-	if (blk_mq_sched_try_insert_merge(q, rq, &free)) {
-		blk_mq_free_requests(&free);
+	if (blk_mq_sched_try_insert_merge(q, rq))
 		return;
-        }
 
-	trace_block_rq_insert(rq);
+	blk_mq_sched_request_inserted(rq);
 
 	if (at_head || blk_rq_is_passthrough(rq)) {
 		if (at_head)
